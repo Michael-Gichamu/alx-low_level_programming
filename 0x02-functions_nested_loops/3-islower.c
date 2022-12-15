60980e7f4c6ed41fb331f4a4881be8819c7ba8ee
Michael-Gichamu <michaelmatere606@gmail.com>
2022-12-14 20:21:04 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - returns 1 if alphabetic character and o if otherwise
+ * _islower - returns 1 if lowercase character and o if otherwise
  *
  * @c: The character is used as a parameter in the function
  *
- * Return: 1 for alphabetic character. 0 for otherwise
+ * Return: 1 for lowercase character. 0 for otherwise
  */
 int _islower(int c)
 {
