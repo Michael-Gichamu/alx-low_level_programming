8ba5eccb243cf4d639fe5050076a874b76d0da5a
Michael-Gichamu <michaelmatere606@gmail.com>
2022-12-14 20:19:57 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * isalpha - returns 1 if alphabetic character and 0 if otherwise
+ *
+ * @c: The character is used as a parameter in the function
+ *
+ * Return: 1 for alphabetic character. 0 for otherwise
+ */
+int _isalpha(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
