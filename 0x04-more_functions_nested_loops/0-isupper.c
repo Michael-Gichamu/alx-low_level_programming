2e5eeb3123b89556da2b2b72ec5cab19071bd18b
Michael-Gichamu <michaelmatere606@gmail.com>
2022-12-15 16:57:16 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * _isuppper - checks for uppercase character
  *
- * @c: This is the parameter of the function
+ * @c: This is the parameter of the function.
  *
  * Return: Returns 1 if c is uppercase and 0 if otherwise
  */
