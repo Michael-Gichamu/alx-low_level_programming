9549b3d8017113149a73dc784c0df6a09ac363c1
Michael-Gichamu <michaelmatere606@gmail.com>
2023-01-17 11:42:19 -0800

    Header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - Dog credits
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: Dog credits.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
+
