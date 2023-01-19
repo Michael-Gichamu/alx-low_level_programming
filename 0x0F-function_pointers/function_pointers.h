87e75d19044e3aeb0b6bb44c1b0edbe3b4a0d541
Michael-Gichamu <michaelmatere606@gmail.com>
2023-01-19 11:43:48 -0800

    Header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef _FPOINTER_H_
-#define _FPOINTER_H_
+#ifndef _FUNCTION_POINTER_H_
+#define _FUNCTION_POINTER_H_
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
